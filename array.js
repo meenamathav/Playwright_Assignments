c56db53144958d229fca2b38ee8c8e4f5897e5e2
meenamathav <157848326+meenamathav@users.noreply.github.com>
2025-12-22 06:38:11 +0000

    Add files via upload

---
+++ array.js
@@ -0,0 +1,18 @@
+let data = ["chrome",true,132.8]
+data[5] = "webkit"
+console.log(data)
+data.pop()
+console.log(data)
+data.push("firefox")
+console.log(data)
+data.shift()
+console.log(data)
+for(newarray of data)
+{
+    console.log(newarray)
+}
+ console.log(" for each" )
+data.forEach((array2)=>
+{
+    console.log( array2)
+})
